d296703d3485a3e8908c5d25457925c69ec94e68
Lucas Marshall <lmarshall@stripe.com>
2025-07-17 11:57:20 -0400

    Update readme and some explorations

--- cube.js
+++ cube.js
@@ -1,4 +1,14 @@
 // Cube configuration options: https://cube.dev/docs/config
 /** @type{ import('@cubejs-backend/server-core').CreateOptions } */
 module.exports = {
+    // some hacking around to try to support multiple merchants
+    // but it's not working yet
+    // contextToAppId: ({ securityContext }) =>
+    //     `CUBE_APP_${securityContext.merchant}`,
+    // contextToOrchestratorId: ({ securityContext }) =>
+    //     `CUBE_APP_${securityContext.merchant}`,
+    // driverFactory: ({ securityContext }) => ({
+    //     type: "sigma",
+    //     apiKey: process.env[`CUBEJS_DB_SIGMA_API_KEY_${securityContext.merchant.toUpperCase()}`],
+    //   })
 };
